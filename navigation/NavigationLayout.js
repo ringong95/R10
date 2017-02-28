6df62fa07af90b4ccd55fefea8086f75c893e321
Ringo <ringong95@gmail.com>
2017-02-28 13:21:42 -0800

    Added icons to nav bar.

--- navigation/NavigationLayout.js
+++ navigation/NavigationLayout.js
@@ -5,23 +5,34 @@ import {
 } from '@exponent/ex-navigation';
 import Icon from 'react-native-vector-icons/Ionicons';
 import React, { Component } from 'react'
+import { View, Text } from 'react-native';
 import About from './../js/scenes/About'
+import styles from './styles.js'
 
 export default class TabScreen extends React.Component {
+  renderIcon(title, iconName, isSelected) {
+    let color = isSelected ? 'white' : '#999999';
+    return (
+      <View style={styles.tabItemContainer}>
+        <Icon style={styles.icon} size={24} color={color} name={iconName} />
+        <Text style={[styles.tabTitleText, { color }]} numberOfLines={1}>
+          {title}
+        </Text>
+      </View>
+    );
+  }
+
   render() {
     return (
       <TabNavigation
+        tabBarColor="black"
         id="root"
         navigatorUID="root"
         initialTab="about">
 
         <TabItem
           id="about"
-          title="About"
-          renderIcon={(isSelected) =>  <Icon name="logo-twitch"/> }>
-          
-          
-          
+          renderIcon={(isSelected) => this.renderIcon('About', 'logo-twitch', isSelected)}>
           <StackNavigation
 
             id="about"
@@ -31,8 +42,7 @@ export default class TabScreen extends React.Component {
         </TabItem>
         <TabItem
           id="schedule"
-          title="Schedule"
-        >
+          renderIcon={(isSelected) => this.renderIcon('Schedule', 'md-calculator', isSelected)}>
           <StackNavigation
             id="schedule"
             navigatorUID="schedule"
---
+++ navigation/styles.js
@@ -0,0 +1,19 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+const styles = StyleSheet.create({
+  tabItemContainer: {
+    flex: 1,
+    alignSelf: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  tabTitleText: {
+    fontSize: 5,
+  },
+  icon:{
+    textAlign: 'center'
+  }
+});
