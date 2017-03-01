5579f9cc9cca68fe2488d30d01b379c833dec3cc
Ringo <ringong95@gmail.com>
2017-03-01 13:29:24 -0800

    Added Realm and Realm model.

---
+++ js/config/model.js
@@ -0,0 +1,10 @@
+import Realm from 'realm';
+const Fave = {
+  name:'Fave',
+  primaryKey: 'id',
+  properties: {
+    id: 'string', // primary key
+    faved_on: 'date'
+  }
+};
+export default new Realm({schema: [Fave]});
