dee7505a2e7f59ad9e22b85e1f2a47576f57f8d9
Ringo <ringong95@gmail.com>
2017-03-02 10:35:06 -0800

    Added back buton to Speaker scene.

--- js/scenes/Speaker/speaker-container.js
+++ js/scenes/Speaker/speaker-container.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Icon from 'react-native-vector-icons/Ionicons';
 import { Text, View, Image, TouchableHighlight, TouchableOpacity } from 'react-native'
 import Speaker from './speaker'
 class SpeakerContainer extends Component {
+  constructor() {
+    super()
+    this.goBack = this.goBack.bind(this);
+  }
+  goBack = () => {
+    this.props.navigator.pop();
+  }
   render() {
     return (
-     <Speaker/>
+      <View>
+        <Text>
+          Hi
+        </Text>
+        <Icon.Button name="ios-play-outline" backgroundColor="#3b5998" onPress={this.goBack}>
+          Login with Facebook
+          </Icon.Button>
+      </View>
     );
   }
 }
-
-export default SpeakerContainer;
+const mapStateToProps = (state) => {
+  return {
+    navigation: state.navigation,
+  };
+};
+export default connect(mapStateToProps)(SpeakerContainer);
--- js/scenes/Speaker/speaker.js
+++ js/scenes/Speaker/speaker.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 import styles from './styles'
 
-const Speaker = () => {
+
+const Speaker = ({navigation}) => {
+  const goBack = () => {
+    console.log("HELO ME")
+    this.props.navigator.pop();
+    console.log("HELO ME")
+  }
   return (
-    <View style={styles.container}>
-    <Text>
-    Hi
+    <View>
+      <Text>
+        Hi
     </Text>
+      <Icon.Button name="ios-play-outline" backgroundColor="#3b5998" onPress={() => goBack()}>
+        Login with Facebook
+  </Icon.Button>
     </View>
   );
 };
